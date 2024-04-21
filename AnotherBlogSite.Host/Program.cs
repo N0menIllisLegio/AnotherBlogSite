8370c33b987d9376c71c4dbc14f954f7ade3b5b7
Dmitry <dmitrymukhliada@gmail.com>
2024-04-21 19:49:06 +0200

    Add CORS

--- AnotherBlogSite.Host/Program.cs
+++ AnotherBlogSite.Host/Program.cs
@@ -8,9 +8,14 @@ builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication(builder.Configuration);
 builder.Services.AddPresentation();
 
+builder.Services.AddCors(opt => opt.AddDefaultPolicy(
+    x => x.WithOrigins("https://localhost:44435").AllowAnyMethod().AllowAnyHeader()));
+
 var app = builder.Build();
 
 await app.Services.ApplyMigrationsAsync();
+
+app.UseCors();
 
 app.UsePresentation();
 
