6a9f54d3a5d309e9c73a9eb32e51d60c8688a232
Dmitry <dmitrymukhliada@gmail.com>
2024-04-21 22:55:28 +0200

    Create BlogPostsService.ts

---
+++ AnotherBlogSite.Presentation/ClientApp/services/BlogPostsService.ts
@@ -0,0 +1,42 @@
+﻿import httpClient from "./RequestProvider.ts";
+import BlogPost from "../models/BlogPost.ts";
+import Guid from "../models/Guid.ts";
+
+export const getBlogPosts = async (): Promise<BlogPost[]> => {
+    const response = await httpClient.get<BlogPost[]>('/BlogPosts');
+    
+    return  response.data;
+}
+
+export const getBlogPost = async (blogPostId: Guid): Promise<BlogPost> => {
+    const response = await httpClient.get<BlogPost>(`/BlogPosts/${blogPostId}`);
+
+    return response.data;
+}
+
+export interface CreateBlogPost {
+    title: string;
+    content: string;
+}
+
+export const createBlogPost = async (createBlogPost: CreateBlogPost): Promise<BlogPost> => {
+    const response = await httpClient.post<BlogPost>('/BlogPosts', createBlogPost);
+
+    return response.data;
+}
+
+export interface UpdateBlogPost {
+    blogPostId: Guid;
+    title: string;
+    content: string;
+}
+
+export const updateBlogPost = async (updateBlogPost: UpdateBlogPost): Promise<BlogPost> => {
+    const response = await httpClient.put<BlogPost>('/BlogPosts', updateBlogPost);
+    
+    return response.data;
+}
+
+export const deleteBlogPost = async (blogPostId: Guid): Promise<void> => {
+    await httpClient.delete<void>(`/BlogPosts/${blogPostId}`);
+}
