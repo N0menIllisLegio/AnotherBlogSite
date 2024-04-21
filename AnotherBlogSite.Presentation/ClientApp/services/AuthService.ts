5e281ee4cc4cd3efb8930e388b3360cc31d421df
Dmitry <dmitrymukhliada@gmail.com>
2024-04-21 22:51:51 +0200

    Add axios and create RequestProvider.ts and AuthService.ts

---
+++ AnotherBlogSite.Presentation/ClientApp/services/AuthService.ts
@@ -0,0 +1,16 @@
+﻿import httpClient from "./RequestProvider.ts";
+
+const TokenKey: string = "AnotherBlogSite-Token";
+
+export const signIn = async (email: string, password: string): Promise<void> => {
+    const response = await httpClient.post<string>("Auth/SignIn", {
+        email,
+        password,
+    });
+    
+    localStorage.setItem(TokenKey, response.data);
+}
+
+export const getToken = (): string | null => localStorage.getItem(TokenKey);
+
+export const signOut = (): void => localStorage.removeItem(TokenKey);
---
+++ AnotherBlogSite.Presentation/ClientApp/services/RequestProvider.ts
@@ -0,0 +1,19 @@
+﻿import axios from "axios";
+import {getToken} from "./AuthService.ts";
+
+const httpClient = axios.create({
+    baseURL: 'https://localhost:7281',
+});
+
+httpClient.interceptors.request.use((config) => {
+    const token = getToken();
+    
+    if (token !== null) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    
+    return config;
+})
+
+
+export default httpClient;
