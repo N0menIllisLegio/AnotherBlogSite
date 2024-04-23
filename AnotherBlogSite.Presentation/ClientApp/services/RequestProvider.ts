d37f3e8ae0f2684c3a2c2b97806ce60e210a7cf4
Dmitry <dmitrymukhliada@gmail.com>
2024-04-23 21:06:38 +0200

    Rework error parsing logic

--- AnotherBlogSite.Presentation/ClientApp/services/RequestProvider.ts
+++ AnotherBlogSite.Presentation/ClientApp/services/RequestProvider.ts
@@ -1,44 +1,58 @@
 ﻿import axios from "axios";
 import {getToken, signOut} from "./AuthService.ts";
-import {ValidationProblemDetails} from "../models/ProblemDetails.ts";
-import {ExtractValidationProblemDetailsMessage, UnknownErrorMessage} from "../utils/ErrorsUtils.ts";
+import {IValidationProblemDetails} from "../models/IProblemDetails.ts";
+import {UnknownErrorMessage} from "../utils/ErrorsUtils.ts";
 
 const httpClient = axios.create({
     baseURL: 'https://localhost:7281',
     transformResponse: (data, _, status) => {
         if (status === 204)
             return null;
-        
-        let result = JSON.parse(data);
-        
-        if (status === 400 || status === 404) {
-            const validationError: ValidationProblemDetails = result;
-            let errorMessage = UnknownErrorMessage;
-            
-            if (validationError) {
-                errorMessage = ExtractValidationProblemDetailsMessage(validationError) ?? errorMessage;
-            }
-            
-            return errorMessage;
-        } else if (status === 401 || status === 403) {
+
+        if (status === 401 || status === 403) {
             signOut();
-            
+
+            // TODO: Rework auth flow
             return "Unauthorized! Please sign in.";
         }
-        
+
+        let result = JSON.parse(data);
+
+        if (status === 400 || status === 404) {
+            const validationError: IValidationProblemDetails = result;
+            let errorMessage = UnknownErrorMessage;
+
+            if (validationError) {
+                const errorsDictionary = validationError?.errors;
+
+                if (errorsDictionary !== undefined) {
+                    let validationErrorMessage = "";
+
+                    for (let key in errorsDictionary) {
+                        validationErrorMessage += errorsDictionary[key].join("\n");
+                        validationErrorMessage += "\n\n";
+                    }
+
+                    if (validationErrorMessage !== "")
+                        errorMessage = validationErrorMessage;
+                }
+            }
+
+            return errorMessage;
+        }
+
         return result;
     }
 });
 
 httpClient.interceptors.request.use((config) => {
     const token = getToken();
-    
+
     if (token !== null) {
         config.headers.Authorization = `Bearer ${token}`;
     }
-    
-    return config;
-})
 
+    return config;
+});
 
 export default httpClient;
--- AnotherBlogSite.Presentation/ClientApp/utils/ErrorsUtils.ts
+++ AnotherBlogSite.Presentation/ClientApp/utils/ErrorsUtils.ts
@@ -1,13 +1,1 @@
-﻿import {ValidationProblemDetails} from "../models/ProblemDetails.ts";
-
-export const UnknownErrorMessage = "Oops unexpected error happened... Please try again later";
-
-export function ExtractValidationProblemDetailsMessage(error: ValidationProblemDetails): string | null {
-    const errorsDictionary = error?.errors;
-    
-    if (errorsDictionary === undefined) return null;
-    
-    const message = errorsDictionary["GeneralError"][0];
-    
-    return message === undefined ? null : message;
-}
+﻿export const UnknownErrorMessage = "Oops unexpected error happened... Please try again later";
