febb6ae30ece06c5ea0905487738021786822d90
Dmitry <dmitrymukhliada@gmail.com>
2024-04-26 03:30:47 +0200

    Merge Host into Presentation

---
+++ AnotherBlogSite.Presentation/Program.cs
@@ -0,0 +1,64 @@
+using AnotherBlogSite.Application;
+using AnotherBlogSite.Data;
+using AnotherBlogSite.Presentation.Middlewares;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddData(builder.Configuration);
+builder.Services.AddApplication(builder.Configuration);
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Description = "JWT bearer authorization.",
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        Type = SecuritySchemeType.ApiKey,
+        In = ParameterLocation.Header,
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new() { Reference = new() { Id = "Bearer", Type = ReferenceType.SecurityScheme } },
+            new string[] { }
+        }
+    });
+});
+
+builder.Services.AddTransient<RequestsLoggingMiddleware>();
+builder.Services.AddControllers();
+
+builder.Services.AddCors(opt => opt.AddDefaultPolicy(
+    x => x.WithOrigins("https://localhost:44435").AllowAnyMethod().AllowAnyHeader()));
+
+var app = builder.Build();
+
+await app.Services.ApplyMigrationsAsync();
+
+app.UseCors();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwaggerUI();
+    app.UseSwagger();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.UseMiddleware<RequestsLoggingMiddleware>();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action=Index}/{id?}");
+
+app.MapFallbackToFile("index.html");
+
+app.Run();
